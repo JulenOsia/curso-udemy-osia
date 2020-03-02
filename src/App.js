6dabcf2d3cba5d5bdb4b0998bae031d965acb418
Julen <julenoi95@gmail.com>
2020-03-02 13:02:24 +0100

    Hasta video 22

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ConditionalSection from './sections/conditional'
 
-class Contador extends Component{
-  constructor(props) {
-    super(props)
-    console.log(this.props.contadorInicial)
-    this.state = {contador: this.props.contadorInicial}
-    setInterval(() => {
-      this.setState({ contador: this.state.contador + 1})
-    }, 1000)
-  } 
-  render () {
-    
-    return <ContadorNumero numero={this.state.contador} />
-  } 
-}
 
-Contador.defaultProps = {
-  contadorInicial: 0
-}
-
-class ContadorNumero extends Component {
-  render () {
-    console.log('ContadorNumero render()')
-    return <span>{this.props.numero}</span>
-  }
-}
 class App extends Component {
     render(){
       return (
         <div className="App">
-          <p>Primer componente con state</p>
-          <Contador contadorInicial={100} />
+          <ConditionalSection />
         </div>
       );
     }
