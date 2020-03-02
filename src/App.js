c63de2df4c214c4125fa769cb6186261d0d1a767
Julen <julenoi95@gmail.com>
2020-03-02 13:37:25 +0100

    Hasta video 25

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import ConditionalSection from './sections/conditional'
-
 
 class App extends Component {
-    render(){
-      return (
-        <div className="App">
-          <ConditionalSection />
-        </div>
-      );
-    }
+  render() {
+    const numbers = [1, 1, 3, 4, 5]
+
+    return (
+      <div className="App">
+        <h4>Trabajando con listas</h4>
+        {numbers.map((number,index) => {
+          return <p key={index}>Soy el numero {number}</p>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
