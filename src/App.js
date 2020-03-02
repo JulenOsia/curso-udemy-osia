6f1988bc3b956da307140b55478a5beed5dc05b5
Julen <julenoi95@gmail.com>
2020-03-02 14:03:49 +0100

    Hasta video 26

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
+import cars from './data/cars.json';
 
+class CarItem extends Component {
+  
+  render () {
+    const { car } = this.props
+
+    return (
+      <li>
+        <p><strong>Nombre: </strong>{car.name}</p>
+        <p><strong>Marca: </strong>{car.company}</p>
+      </li>
+      /*
+      const { car, key } = this.props
+      return (
+      <li>
+        <p>Key: {key}</p>
+      </li>
+      )
+      No mostrará la key en pantalla pero si un mensaje de error en la consola
+      indicando que la key solo se usa de forma interna en React
+      */
+    )
+  }
+}
 class App extends Component {
   render() {
-    const numbers = [1, 1, 3, 4, 5]
 
     return (
       <div className="App">
-        <h4>Trabajando con listas</h4>
-        {numbers.map((number,index) => {
-          return <p key={index}>Soy el numero {number}</p>
-        })}
+        <h4>Trabajando con listas con objetos</h4>
+        <ul>
+          {
+            cars.map(car =>{
+              return (
+                <CarItem key={car.id} car={car} />
+
+                /* Para mostrar la key hay que pasar el valor en una prop diferente
+                Ejemplo de este caso:
+                const { car, id } = this.props
+                <p>Key: {id}</p>
+                return <CarItem id={car.id} key={car.id} car={car} */
+              )
+            })
+          }
+        </ul>
       </div>
     );
   }
