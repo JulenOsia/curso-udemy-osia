bc24e88ff3b231d3ea98d6f831a688f15d8cbc00
Julen <julenoi95@gmail.com>
2020-03-04 13:49:13 +0100

    Hasta video 32

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  handleClick (e) {
+    console.log(e)
+    console.log(e.nativeEvent)
+    alert('Hi here!')
+  }
   render() {
     return (
       <div className="App">
         <h4>Eventos</h4>
-          <button onClick={() => alert('Hi here!')}>Hi there!</button>
+          <button onClick={this.handleClick}>Hi there!</button>
       </div>
     );
   }
