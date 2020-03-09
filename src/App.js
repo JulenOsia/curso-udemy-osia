c7df36bd7bc84a817825ef593e7e71d95bb13968
Julen <julenoi95@gmail.com>
2020-03-09 13:20:31 +0100

    Hasta video 42 (PropTypes)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-
-class Box extends Component {
-  render () {
-    return (
-      <div style={{border: '1px solid #000', margin: 5, padding: 5 }}>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
+import PropTypes from 'prop-types'
 class Article extends Component {
+  static propTypes = {
+    author: PropTypes.string.isRequired
+  }
+
   render () {
+    const { author, children, date, title } = this.props
     return (
       <section>
-        <h2>{this.props.title}</h2>
-        <p><em>Escrito por {this.props.author}</em></p>
-        <Box>{this.props.date}</Box>
+        <h2>{title}</h2>
+        {author &&  <p><em>Escrito por {author}</em></p>}
+        <date>{date}</date>
         <article>
-          {this.props.children}
+          {children}
         </article>
       </section>
     )
@@ -31,20 +26,11 @@ class App extends Component {
       <div className="App">
         <h4>Children props</h4>
         <Article
-          author='Julen'
+          
           date={new Date().toLocaleDateString()}
           title='Articulo sobre la prop children'
         >
-          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children.</p>
-            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
-
-        </Article>
-        <Article
-          author='Peio'
-          date={new Date().toLocaleDateString()}
-          title='Articulo sobre la prop children'
-        >
-          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children.</p>
+          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como children.</p>
             <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
 
         </Article>
